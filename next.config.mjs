4880ff9c213e27967eee21397e982b2e18205faf
Amr Tanany <tanany95175325@gmail.com>
2026-09-09 18:02:05 +0300

    Add basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/apex-nine-racing',
   allowedDevOrigins: ['127.0.0.1'],
   typescript: {
     ignoreBuildErrors: true,
